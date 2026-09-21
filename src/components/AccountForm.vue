bade1a2545928f1a710a6499eec24a090d2db7a4
Steffen Schmitt <steffen@schmitt.codes>
2026-09-21 18:31:29 +0200

    feat: show a placeholder in the token field when editing an account that has one

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -87,9 +87,9 @@ function submit() {
         type="password"
         autocomplete="off"
         spellcheck="false"
-        class="mt-1 w-full rounded border px-2 py-1 font-mono dark:border-slate-700 dark:bg-slate-800"
+        :placeholder="account?.token ? t('accounts.tokenSet') : ''"
+        class="mt-1 w-full rounded border px-2 py-1 font-mono placeholder:font-sans dark:border-slate-700 dark:bg-slate-800"
       />
-      <span v-if="account" class="text-xs text-slate-500">{{ t('accounts.tokenKeep') }}</span>
       <RouterLink to="/help" class="ml-2 text-xs text-sky-700 underline dark:text-sky-400">{{ t('accounts.howTo') }}</RouterLink>
     </label>
     <label class="block text-sm">
